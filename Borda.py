1e7a159041d8f53904c8471d369a74f7d0571c16
WangPeng <wangpeng930204@gmail.com>
2021-11-10 11:22:55 +0100

    changed by WangPeng at 11.10

--- Borda.py
+++ Borda.py
@@ -1,15 +1,44 @@
 import pandas as pd
+import numpy as np
+
 
 class BordaVoting:
 
+    # Compute the social ranking of candidates, eg : [A, B, C, D]
     def outcomeRanking(self, preferences: pd.DataFrame) -> [str]:
-        # Compute the social ranking of candidates, eg : [A, B, C, D]
-        pass
+        rank = {}
+        info = np.array(preferences).transpose()[2:].transpose().tolist()
+        for num_candidates in range(len(info)):
+            for candidate in info[num_candidates]:
+                if candidate in rank.keys():
+                    rank[candidate] += rank[candidate] + len(info) - num_candidates
+                else:
+                    rank[candidate] = len(info) - num_candidates
+        outcome_rank = list(rank.keys())
+        return outcome_rank
 
-    def outcome(self, preferences: pd.DataFrame) -> str:
-        # Compute the final outcome of the vote, eg : A
-        pass
+    def get_outcome(self, preferences: pd.DataFrame) -> str:
+        info = np.array(preferences).transpose()[2:].transpose().tolist()
+        outcome = self.outcomeRanking(info)[0]
+        return outcome
 
-    def happiness(self, preferences: pd.DataFrame, outcome: str) -> [float]:
-        # Compute the happiness of all voters based on a given outcome
-        pass
+    # Compute the happiness of all voters based on a given outcome
+    def happiness(self, preferences: pd.DataFrame, outcome: [str]) -> [float]:
+        info = np.array(preferences).transpose()[2:].transpose().tolist()
+        can_num = len(info)
+        voter_num = len(info[0])
+        happiness = [0 for i in range(voter_num)]
+        for i in range(can_num):
+            for j in range(voter_num):
+                delta = i - outcome.index(info[i][j])
+                happiness_weight = can_num - i
+                happiness[j] = happiness[j] + delta * happiness_weight
+        return happiness
+
+
+if __name__ == "__main__":
+    inputFile = "DataSet.csv"  # for future command line IO
+    preferences = pd.read_csv(inputFile)
+    b = BordaVoting()
+    outcome = b.outcomeRanking(preferences)
+    print(b.happiness(preferences, outcome))
