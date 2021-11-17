8522370ca3e62ce997ea369f25bdc6388dba4c29
gMontymk33 <geminimontyjr@hotmail.com>
2021-11-17 18:37:47 +0100

    Updated Happiness, fixed VotingForTwo mistake

--- VotingForTwo.py
+++ VotingForTwo.py
@@ -6,7 +6,7 @@ class VotingForTwo:
     def outcomeRanking(self, preferences: pd.DataFrame) -> [str]:
         # Compute the social ranking of candidates, eg : [A, B, C, D]
 
-        voterno = preferences.shape[1] - 1
+        voterno = preferences.shape[1]
         # In case NO VOTES were cast for a specific candidate in either of the two preferences,
         # there's an error, as the series cannot include an index value not present in both
         a = preferences.iloc[0].value_counts(
@@ -16,16 +16,16 @@ class VotingForTwo:
         a = a.sort_values(ascending=False)
         # print(a.index)
         # print(list(a))
-        return list(a)
+        return list(a.index.tolist())
 
     def outcome(self, preferences: pd.DataFrame) -> str:
         outcome = self.outcomeRanking(preferences)
-        most_voted_votes = list(outcome)[0]
-        if most_voted_votes / preferences.shape[1] - 1 > 0.5:
-            return outcome.index[0]
-        else:
-            return "None"
-        return
+        # most_voted_votes = list(outcome)[0]
+        # if most_voted_votes / preferences.shape[1] - 1 > 0.5:
+        #     return outcome.index[0]
+        # else:
+        #     return "None"
+        return outcome[0]
 
     def happiness(self, preferences: pd.DataFrame, outcome: str) -> [float]:
         # Compute the happiness of all voters based on a given outcome
--- run.py
+++ run.py
@@ -10,38 +10,87 @@ inputFile = "example.csv"  # for future command line IO
 preferences = pd.read_csv(inputFile)
 
 # Choose the voting scheme
-votingOptions = [BordaVoting(), PluralityVoting(), AntiPluralityVoting(), VotingForTwo()]
-votingSchemeChoice = 0  # for future command line IO
+votingOptions = [BordaVoting(), PluralityVoting(),
+                 AntiPluralityVoting(), VotingForTwo()]
+votingSchemeChoice = 3  # for future command line IO
 votingScheme = votingOptions[votingSchemeChoice]
 
 # Define happiness function
-def happiness(voter : str, preferences, outcome: [str]) -> float:
+
+# for future command line IO  0 = Steep, 1 = Middle, 2 = Linear ect...
+happinessMetricOptions = 0
+
+
+def happiness(voter: str, preferences, outcome: [str], happinessMetric: int) -> float:
     """
-    :param voter: string representing the voted, eg : "Voter 1"
+    :param voter: string representing the voted, eg : "Voter 1"  *Giannis comment - Why string and not index value?
     :param preferences: pandas dataframe with the voting preferences
     :param outcome: social ranking, eg : [A, B, C, D]
     :return: the happiness of a given voter with a given outcome
     """
-    pass
+
+    # retrieve the preferences of the current voter
+    a = preferences.loc[:, voter].tolist()
+
+    # calculate the number of candidates/voting options
+    n = len(outcome)
+    # calculate how many steps the winner is removed from the current voter's preference list
+    s = a.index(outcome[0])
+
+    # For each happinessMetric apply a different happiness calculation function. Max happiness = 1
+    if happinessMetric == 0:
+        # Happiness is defined on a steep curve and is halved for every step of removal.
+        return n/((2 ** s)*n)
+    elif happinessMetric == 1:
+        # Happiness is defined on a linear curve
+        return (n-s)/n
+    elif happinessMetric == 2:  # Needs debugging!!
+        # Happiness is defined on a middle curve. First step is halved, the rest linear.
+        if s == 0:
+            return (n*0.5 + (n-s)*0.5) / n
+        else:
+            return ((n-s)*0.5) / n
+    else:
+        return 0
+
 
 # Define overall happiness
-def overallHappiness(preferences, outcome: [str]) -> float:
+
+
+def overallHappiness(preferences, outcome: [str], happinessMetric) -> float:
     """
     :param preferences: pandas dataframe with the voting preferences
     :param outcome: social ranking, eg : [A, B, C, D]
     :return:
     """
     # sum the happiness of all individual voters
-    pass
+
+    # initialise variable that holds total happiness sum
+    summedHappiness = 0
+
+    # calculate happiness for each individual voter and add the result to the sum
+    for voter in preferences.columns:
+        # Print is only for visualisation, should be removed. Happiness is calculated two times on purpose since next line is reduntant.
+        print("Happiness of :", voter, " is :", happiness(voter,
+                                                          preferences, outcome, happinessMetric))
+        # Sum total happiness.
+        summedHappiness = summedHappiness + happiness(voter,
+                                                      preferences, outcome, happinessMetric)
+
+    # return normalised happiness
+    return summedHappiness/len(preferences.columns)
 
 # Define strategic voting function
-def strategicVoting(voter : str, preferences, votingScheme) :
+
+
+def strategicVoting(voter: str, preferences, votingScheme):
     """
     :param voter: string representing the voted, eg : "Voter 1"
     :param preferences: pandas dataframe with the voting preferences
     :param votingScheme: initialized voting scheme
     :return: a strategic voting option of the form :
-             [newVoterRanking, outcome, newHappiness, newOverallHappiness, trueOverallHappiness]
+             [newVoterRanking, outcome, newHappiness,
+                 newOverallHappiness, trueOverallHappiness]
     """
     voterRanking = preferences[voter]
     permutations = list(itertools.permutations(list(voterRanking)))
@@ -50,7 +99,8 @@ def strategicVoting(voter : str, preferences, votingScheme) :
         newPreferences = preferences.copy()
         newPreferences[voter] = alternative
         outcomeRanking = votingScheme.outcomeRanking(newPreferences)
-        option = [alternative, votingScheme.outcome(newPreferences), happiness(voter, newPreferences, outcomeRanking), overallHappiness(newPreferences, outcomeRanking), overallHappiness(preferences, outcomeRanking)]
+        option = [alternative, votingScheme.outcome(newPreferences), happiness(voter, newPreferences, outcomeRanking), overallHappiness(
+            newPreferences, outcomeRanking), overallHappiness(preferences, outcomeRanking)]
         strategies.append(option)
     return strategies
 
@@ -58,4 +108,6 @@ def strategicVoting(voter : str, preferences, votingScheme) :
 # TEST THINGS HERE
 print("Social Ranking:", votingScheme.outcomeRanking(preferences))
 print("Voting Winner:", votingScheme.outcome(preferences))
-print(strategicVoting("Voter 1", preferences, votingScheme))
+# print(strategicVoting("Voter 1", preferences, votingScheme))
+print("Happiness of voters :", overallHappiness(
+    preferences, votingScheme.outcomeRanking(preferences), happinessMetricOptions))
