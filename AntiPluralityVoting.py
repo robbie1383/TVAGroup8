34ccea36a2d6f93ab181046bf58f14702656402b
Ntantis, Nick (Stud. DKE) <n.ntantis@student.maastrichtuniversity.nl>
2021-11-18 14:15:59 +0100

    first column bug fixed

--- AntiPluralityVoting.py
+++ AntiPluralityVoting.py
@@ -3,24 +3,25 @@ import pandas as pd
 class AntiPluralityVoting:
 
     def outcomeRanking(self, preferences: pd.DataFrame) -> [str]:
-        # Compute the social ranking of candidates, eg : [A, B, C, D]
-
-        # initializer a ranking dictionary with zeros ( preferences are found in the first voter)
+        '''
+        :param preferences: pandas dataframe with the voting preferences
+        :return: final ranking eg : [A,B,C,D]
+        '''
 
         #initializer ranking dictionary with zeros
-        ranking = dict.fromkeys(preferences.iloc[:,1].values, 0) #start from 1 because 1 is preference
+        ranking = dict.fromkeys(preferences.iloc[:, 0].values, 0)
 
         # dictionary with scores
         first = True
         for voter in preferences._iter_column_arrays():
             if first:
-                first=False
+                first = False
                 continue
             for i in range(len(voter) - 1):
                 ranking[voter[i]] += 1
 
         # sort first by score then alphabetically
-        '''Note that we have here a sorted dictionary. It might become very usefull in the future'''
+        '''Note that we have here a sorted dictionary. It might become very useful in the future'''
         ranking = dict(sorted(ranking.items(), key=lambda x: (-x[1], x[0]), reverse=False))
 
         #get only the preferences and not the equivalent votes
@@ -29,6 +30,10 @@ class AntiPluralityVoting:
         return outcomeRanking
 
     def outcome(self, preferences: pd.DataFrame) -> str:
+        '''
+        :param preferences: pandas dataframe with the voting preferences
+        :return: single winner
+        '''
         # Compute the final outcome of the vote, eg : A
         score = self.outcomeRanking(preferences)
         return score[0]
