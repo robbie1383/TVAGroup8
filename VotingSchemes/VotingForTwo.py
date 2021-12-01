6b2f3dc34558d5af3d54200a31378abaa814ffe7
gMontymk33 <geminimontyjr@hotmail.com>
2021-12-01 19:57:16 +0100

    Fixed VotingForTwo

--- VotingSchemes/VotingForTwo.py
+++ VotingSchemes/VotingForTwo.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import operator
 
 
 class VotingForTwo:
@@ -6,11 +8,18 @@ class VotingForTwo:
     def outcomeRanking(self, preferences: pd.DataFrame) -> [str]:
         # Compute the social ranking of candidates, eg : [A, B, C, D]
         # Count for each candidate how many times they are in 1st or 2nd place
-        a = preferences.iloc[0].value_counts() + preferences.iloc[1].value_counts()
-        # If a candidate isn't in both, set their votes as 0 and not NaN.
-        a = a.fillna(0)
-        a = a.sort_values(ascending=False)
-        return list(a.index.tolist())
+        prefarray = np.array(preferences)
+        ranking = dict.fromkeys(preferences.iloc[:, 1].values, 0)
+        for i in range(len(prefarray[0])):
+            ranking[prefarray[0][i]] += 1
+
+        for i in range(len(prefarray[1])):
+            ranking[prefarray[1][i]] += 1
+
+        ranking = dict(
+            sorted(ranking.items(), key=operator.itemgetter(1), reverse=True))
+
+        return list(ranking.keys())
 
     def outcome(self, preferences: pd.DataFrame) -> str:
         # Compute the final outcome of the vote, eg : A
