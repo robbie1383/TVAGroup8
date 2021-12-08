73e8b78e1e3bfa70a6b98b43502988cb76a9e402
Elena Kane <59971317+elenakelly@users.noreply.github.com>
2021-12-08 16:21:28 +0100

    Update Borda.py
    outputScores(preferences) -> dictionary

--- VotingSchemes/Borda.py
+++ VotingSchemes/Borda.py
@@ -7,7 +7,7 @@ import numpy as np
 class BordaVoting:
 
     # Compute the social ranking of candidates, eg : [A, B, C, D]
-    def outcomeRanking(self, preferences: pd.DataFrame) -> [str]:
+    def outputScores(self, preferences: pd.DataFrame) -> [str]:
         ranking = {}
         info = np.array(preferences)
         for num_candidates in range(len(info)):
@@ -17,9 +17,13 @@ class BordaVoting:
                 else:
                     ranking[candidate] = len(info) - num_candidates
         ranking = dict(sorted(ranking.items(), key=operator.itemgetter(1), reverse=True))
+        return ranking
+
+    def outcomeRanking(self, preferences: pd.DataFrame) -> [str]:
+        ranking = self.outputScores(preferences)
         outcome_rank = list(ranking.keys())
         return outcome_rank
-
+    
     # Compute the final outcome of the vote, eg : A
     def outcome(self, preferences: pd.DataFrame) -> str:
         return self.outcomeRanking(preferences)[0]
