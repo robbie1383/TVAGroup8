e1d70ea600d16692b1e789f1a270569cc54085e5
Nikolaos Ntantis <nickd_96@hotmail.com>
2021-12-08 16:53:04 +0100

    Update AntiPluralityVoting.py

--- VotingSchemes/AntiPluralityVoting.py
+++ VotingSchemes/AntiPluralityVoting.py
@@ -4,26 +4,26 @@ import pandas as pd
 class AntiPluralityVoting:
 
     def outcomeRanking(self, preferences: pd.DataFrame) -> [str]:
+
+        ranking = self.outputScores(preferences)
+        # get only the preferences and not the equivalent votes
+        outcomeRanking = list(ranking.keys())
+        return outcomeRanking
+
+    def outputScores(self, preferences: pd.DataFrame) -> [str]:
         # Compute the social ranking of candidates, eg : [A, B, C, D]
 
         # initialize a ranking dictionary with zeros ( preferences are found in the first voter)
-        ranking = dict.fromkeys(preferences.iloc[:, 1].values, 0)  # start from 1 because 1 is preference
+        ranking = dict.fromkeys(preferences.iloc[:, 0].values, 0)  # start from 1 because 1 is preference
         # dictionary with scores
         first = True
         for voter in preferences._iter_column_arrays():
-            if first:
-                first = False
-                continue
             for i in range(len(voter) - 1):
                 ranking[voter[i]] += 1
 
         # sort first by score then alphabetically
-        '''Note that we have here a sorted dictionary. It might become very usefull in the future'''
         ranking = dict(sorted(ranking.items(), key=lambda x: (-x[1], x[0]), reverse=False))
-        # get only the preferences and not the equivalent votes
-        outcomeRanking = list(ranking.keys())
-
-        return outcomeRanking
+        return ranking
 
     def outcome(self, preferences: pd.DataFrame) -> str:
         # Compute the final outcome of the vote, eg : A
@@ -31,4 +31,4 @@ class AntiPluralityVoting:
         return score[0]
 
     def toString(self):
-        return "Anti-Plurality Voting Scheme"
+        return "Anti Plurality Voting Scheme
